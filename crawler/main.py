813d26731eb2466712ad703410ec77e8ec05f6c1
LuchnikKek <ilua.kabeshov01@mail.ru>
2024-04-06 16:33:32 +0300

    Черновик. Переработал запуск. Теперь начали запускаться несколько отдельных браузеров. Но благодаря одним и тем же страницам, работает хорошо.

--- crawler/main.py
+++ crawler/main.py
@@ -1,12 +1,37 @@
 import asyncio
 
+from playwright.async_api import TimeoutError
+
 from crawler.src.core.browser import browser_context
 from crawler.src.core.config import settings, logger
 from crawler.src.scraper import scrape_video_ids
-from src.utils.bounded_task_group import BoundedTaskGroup
+
+
+async def worker(queue_in, queue_out):
+    async with browser_context() as browser:
+        page = await browser.new_page()
+
+        while True:
+            channel_link = await queue_in.get()
+            try:
+                await page.goto(channel_link)
+            except TimeoutError:
+                pass
+            await page.wait_for_timeout(settings.browser.PAGE_ADDITIONAL_LOADING_TIME_MS)
+            ids = await scrape_video_ids(page, channel_link)
+
+            queue_out.put_nowait(ids)
+            logger.info(ids)
+            queue_in.task_done()
 
 
 async def main():
+    # async with browser_context() as browser:
+    #     tasks = [scrape_video_ids(browser, link) for link in links]
+    #     async with BoundedTaskGroup(max_tasks=settings.browser.MAX_PAGES) as tg:
+    #         results = [tg.create_task(task) for task in tasks]
+    # logger.info("Все задачи завершены. Количество: %s." % len(results))
+
     links = [
         'https://www.youtube.com/@raily',
         "https://www.youtube.com/@adorplayer",
@@ -19,13 +44,17 @@ async def main():
         "https://www.youtube.com/@diodand",
         "https://www.youtube.com/@nedohackerslite",
     ]
+    queue = asyncio.Queue()
+    for link in links:
+        queue.put_nowait(link)
 
-    async with browser_context() as browser:
-        tasks = [scrape_video_ids(browser, link) for link in links]
-        async with BoundedTaskGroup(max_tasks=settings.browser.MAX_PAGES) as tg:
-            results = [tg.create_task(task) for task in tasks]
+    output_queue = asyncio.Queue()
 
-    logger.info("Все задачи завершены. Количество: %s." % len(results))
+    page_workers = []
+    for i in range(settings.browser.MAX_PAGES):
+        page_worker = asyncio.create_task(worker(queue, output_queue))
+
+    await queue.join()
 
 
 if __name__ == '__main__':
